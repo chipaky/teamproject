f45ca3a05a52fde56121c10b00861233b9196a50
quangcn <quangcn@10.117.210.238>
2019-04-03 16:57:32 -0700

    quang updated the file

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("214531370 - This line was added by Samantha Galang.");
 		System.out.println("214531370 - This line was added by Samantha Galang.");
 		System.out.println("219387646 - This line was added by Yongfen Lei.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("215539000 - This line was added by Quang Nguyen.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
