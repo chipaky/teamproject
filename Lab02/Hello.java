1d1f03db2de7e1db2888ddc276ebf3226e7662d5
yongfenlei <49255781+yongfenlei@users.noreply.github.com>
2019-04-03 16:45:12 -0700

    Update Hello.java

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("219869040 - This line was added by Yakov Chipak.");
 		System.out.println("215539000 - This line was added by Quang Nguyen.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("219487646 - This line was added by Yongfen Lei.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
