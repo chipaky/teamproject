62d2e76751246fdaa5623f4423c3c503750f1273
Samantha <Samantha@10.117.105.154>
2019-04-03 17:14:09 -0700

    accidentally put info twice, deleted duplicate info

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -8,7 +8,6 @@ public class Hello {
 		System.out.println("219869040 - This line was added by Yakov Chipak.");
 		System.out.println("219482758 - This line was added by Michelle Wu.");
 		System.out.println("214531370 - This line was added by Samantha Galang.");
-		System.out.println("214531370 - This line was added by Samantha Galang.");
 		System.out.println("219387646 - This line was added by Yongfen Lei.");
 		System.out.println("215539000 - This line was added by Quang Nguyen.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
